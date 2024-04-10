a94ef983c473644be93c2f78279bc0fad51b869e
VikramDevireddy <vikramdevireddy888@gmail.com>
2024-04-10 22:46:41 +0530

    second

--- home.js
+++
@@ -1,9 +0,0 @@
-import Example from "./navbar";
-import Cads from "./newproductlist";
-export default function Home(){
-    return(
-        <div>
-            <Example/>
-             <Cads/>        </div>
-    )
-}
